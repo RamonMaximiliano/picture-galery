d9656c1502b3a7c777eab54cdad7ee4ff51c4a45
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-08-13 13:38:53 -0300

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      ...
+
+    </>
   );
 }
 
 export default App;
+
+
+
+/* 
+
+TO DO:
+
+- Check about installing the uuid library, if it will be necessary or not (not installed yet)
+- Firebase version (not installed yet)
+
+
+Books - 2:25: 
+https://www.youtube.com/watch?v=oT-feDPuJmk&list=PLvRPaExkZHFlFewlz71TJRheerCt4xc9K&index=6&ab_channel=VidaMRR-Dise%C3%B1oydesarrolloweb
+
+
+
+Boni - 6:05
+https://www.youtube.com/watch?v=ss4BXa-WfgI&ab_channel=BoniekyLacerda
+
+
+ */
