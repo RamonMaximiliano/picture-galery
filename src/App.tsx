19c2277880ad2c249e13aeb7333cbb737927f725
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-08-14 22:57:33 -0300

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,23 @@ TO DO:
 - Check about installing the uuid library, if it will be necessary or not (not installed yet)
 - Firebase version (not installed yet)
 
+3 MAIN APP PAGES:
+- Home page which displays the pictures
+- Add page, to create a new picture structure with comments, title
+- Single page, the one which shows the picture in larger size with its comment details
 
-Books - 2:25: 
+
+
+
+Books - 2:29: 
 https://www.youtube.com/watch?v=oT-feDPuJmk&list=PLvRPaExkZHFlFewlz71TJRheerCt4xc9K&index=6&ab_channel=VidaMRR-Dise%C3%B1oydesarrolloweb
 
 
 
-Boni - 6:05
+Boni - 8:10
 https://www.youtube.com/watch?v=ss4BXa-WfgI&ab_channel=BoniekyLacerda
+
+
 
 
  */
