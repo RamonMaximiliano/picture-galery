c44e5cb713757c717185fe36ea8951afb15307a6
Ramon <ramon_max1986@hotmail.com>
2022-08-16 13:23:47 -0300

    loading picture

--- src/components/Add/index.tsx
+++ src/components/Add/index.tsx
@@ -1,6 +1,41 @@
 import './styles.css'
+import {useState} from 'react'
 
 export const Add = () => {
+    const [pict, setPicture] = useState<string>()
+
+
+function createImage(event:any){
+    const image = event.target;
+    const file = image.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file)
+
+    reader.onloadend = function () {
+        setPicture(reader?.result?.toString());
+    };
+}
+
+console.log(pict)
+console.log(crypto.randomUUID())
+
+/* 
+ABOUT THE ABOVE QUESTION MARKS:
+The question mark dot (?.) syntax is called optional chaining in TypeScript.
+This approach is commonly used when fetching data from a remote API or reading data from a file, where some of the properties might not have a value.
+
+"reader.onloadend = function () {
+    setPicture(reader?.result?.toString());
+};"
+
+
+RANDOM UUID:
+To create random Unique IDs:
+console.log(crypto.randomUUID())
+
+*/
+
     return (
         
             <div className="main-add-div">
@@ -8,12 +43,19 @@ export const Add = () => {
                 <input type="text" name="Title" className="Title"/>
                 <h3>Comment</h3>
                 <input type="text" name="Description" className="Description"/>
-                <input type="file" name="picture" className="picture"/>
+                <input type="file" name="picture" className="picture" onChange={createImage}/>
                 <input type="button" value="Add picture" className="Add"/>
+                <p>Preview</p>
+                <div>
+{/*                     If pict state has something then it will show, otherwise: "Waiting for a picture"
+ */}                    {!!pict ? <img src={pict} alt="Preview" />: "Waiting for a picture..."}
+                </div>
             
             </div>
      )
 }
+
+
 
 
 
